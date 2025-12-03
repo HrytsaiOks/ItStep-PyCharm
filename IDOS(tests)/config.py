b21523174575c23cf6185cffbdbddee6f026b7fb
Oksana <ksuha707070@gmail.com>
2025-12-03 21:05:33 +0100

    IDOS_tests

---
+++ IDOS(tests)/config.py
@@ -0,0 +1,19 @@
+Base_Url = "https://idos.cz/vlakyautobusymhdvse/spojeni/"
+
+input_email = 'ksuha707070@gmail.com'
+input_password = 'Praha*777*'
+user_name = 'Oksana H.'
+
+input_mesto_1 = 'Praha'
+input_mesto_2 = 'Brno'
+
+TOP_35_CITIES = [
+    ("Praha", "Pra"), ("Brno", "Brn"), ("Ostrava", "Ost"), ("Plzeň", "Plz"), ("Liberec", "Lib"),
+    ("Olomouc", "Olo"), ("Ústí nad Labem", "Ust"), ("Hradec Králové", "Hra"), ("České Budějovice", "Ces"),
+    ("Pardubice", "Par"), ("Zlín", "Zli"), ("Havířov", "Hav"), ("Kladno", "Kla"), ("Most", "Mos"),
+    ("Opava", "Opa"), ("Frýdek-Místek", "Fry"), ("Karviná", "Kar"), ("Jihlava", "Jih"),
+    ("Teplice", "Tep"), ("Chomutov", "Cho"), ("Děčín", "Dec"), ("Karlovy Vary", "Kar"), ("Třinec", "Tri"),
+    ("Přerov", "Pre"), ("Tábor", "Tab"), ("Znojmo", "Zno"), ("Prostějov", "Pro"), ("Kolín", "Kol"),
+    ("Mladá Boleslav", "Mla"), ("Trutnov", "Tru"), ("Příbram", "Pri"), ("Cheb", "Che"), ("Orlová", "Orl"),
+    ("Kroměříž", "Kro"), ("Vsetín", "Vse")
+]
